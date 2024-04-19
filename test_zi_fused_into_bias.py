1437d9de5134fc086085070b8735188f699d00af
ShengYuanShen <shengyuan.shen@timesintelli.com>
2024-04-19 12:00:21 +0800

    correct

--- test_zi_fused_into_bias.py
+++ test_zi_fused_into_bias.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 
 
@@ -11,21 +12,28 @@ qx = torch.round(x / si) + zi
 qx = torch.clamp(qx, -128, 127)
 
 x1 = qx - zi
-conv = torch.nn.Conv2d(32, 64, kernel_size=3, padding=1)
+conv = torch.nn.Conv2d(32, 64, kernel_size=3, padding=0)
 conv.bias.data.fill_(0.0)
 
 sk = torch.max(torch.abs(conv.weight.data)) / 127.0
 conv.weight.data = torch.round(conv.weight.data / sk)
 conv.weight.data = conv.weight.data.clamp(-128, 127)
-y1 = conv(x1)
-print(y1.sum())
+
+conv_pad = nn.ConstantPad2d((1), -zi)
+
+y1 = conv(conv_pad(x1))
+print(y1.shape, y1.sum())
 
 conv_fused = torch.nn.Conv2d(32, 64, kernel_size=3, padding=1)
 conv_fused.weight.data = conv.weight.data
 conv_fused.bias.data *= 0.0
-# conv_fused.bias.data -= torch.sum(F.conv2d(torch.ones_like(x1) * zi, weight=conv.weight.data, bias=None, padding=1), dim=(2,3)).reshape(-1)
-zi_bias = F.conv2d(torch.ones_like(x1) * zi, weight=conv.weight.data, bias=None, padding=1)
-
+conv_pad_ = nn.ConstantPad2d((1), zi)
 x2 = qx.detach()
-y2 = conv_fused(x2) - zi_bias
-print(y2.sum())
+
+conv_fused.bias.data -= torch.mean(F.conv2d(conv_pad_(torch.ones_like(x1) * zi), weight=conv.weight.data, bias=None, padding=0), dim=(2,3)).reshape(-1)
+y2 = conv_fused(x2)
+
+# zi_bias = F.conv2d(torch.ones_like(x1) * zi, weight=conv.weight.data, bias=None, padding=0)
+# y2 = conv_fused(x2) - zi_bias
+
+print(y2.shape, y2.sum())
